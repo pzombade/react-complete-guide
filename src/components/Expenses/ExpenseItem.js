dda8bcc0e6488d004410ed9dcaee4edc93c7551d
pzombade <prashantzombade@gmail.com>
2023-04-08 23:32:33 +0530

    Added temporary change title feature

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,26 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) =>  {
 
-    // const expenseTitle = 'Car Insurance';
-    // const exponseDate = new Date(2021,2,28);
-    // const exponsePrice = 294.67;
 
- 
-    console.log(">> "+JSON.stringify(props));
+    const [title, setTitle] = useState(props.title);
+
+    const clickChangeTitleHandler = ()=>{
+        setTitle('My New Title!');
+        console.log('clickChangeTitleHandler!!! ' + props.title);
+    };
+    
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={clickChangeTitleHandler}>Change Title</button>
         </Card>
     );
 }
