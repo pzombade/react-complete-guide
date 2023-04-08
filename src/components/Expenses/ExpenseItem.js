8f9fbac7873eb3961f237f7330980f3137d6eba4
pzombade <prashantzombade@gmail.com>
2023-04-08 19:14:33 +0530

    Add all the files

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+    // const expenseTitle = 'Car Insurance';
+    // const exponseDate = new Date(2021,2,28);
+    // const exponsePrice = 294.67;
+
+ 
+    console.log(">> "+JSON.stringify(props));
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </Card>
+    );
+}
+
+
+export default ExpenseItem;
